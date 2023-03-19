b36d0f99d2d653ce4d4b037fb70512a67452a35a
AJ Johnson <ajjohnson@AJs-MacBook-Pro.local>
2023-03-19 11:18:47 -0400

    3D scene imported

--- Experience/Camera.js
+++ Experience/Camera.js
@@ -36,6 +36,15 @@ export default class Camera {
       100
     );
 
+    const size = 20;
+    const divisions = 20;
+
+    const gridHelper = new THREE.GridHelper(size, divisions);
+    this.scene.add(gridHelper);
+
+    const axesHelper = new THREE.AxesHelper(10);
+    this.scene.add(axesHelper);
+
     this.scene.add(this.orthographicCamera);
   }
 
