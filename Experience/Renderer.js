fe237b633b53c11ee2ee49e29d5514a4049973b0
AJ Johnson <ajjohnson@AJs-MacBook-Pro.local>
2023-03-24 12:15:45 -0400

    working slanted images

--- Experience/Renderer.js
+++ Experience/Renderer.js
@@ -36,22 +36,21 @@ export default class Renderer {
     this.renderer.setViewport(0, 0, this.sizes.width, this.sizes.height);
     this.renderer.render(this.scene, this.camera.orthographicCamera);
 
-    this.renderer.setScissorTest(true);
-    this.renderer.setViewport(
-      this.sizes.width - this.sizes.width / 3,
-      this.sizes.height - this.sizes.height / 3,
-      this.sizes.width / 3,
-      this.sizes.height / 3
-    );
-
-    this.renderer.setScissor(
-      this.sizes.width - this.sizes.width / 3,
-      this.sizes.height - this.sizes.height / 3,
-      this.sizes.width / 3,
-      this.sizes.height / 3
-    );
-    this.renderer.render(this.scene, this.camera.perspectiveCamera);
-
-    this.renderer.setScissorTest(false);
+    //create a viewport for the perspective camera and scissor for the orthographic camera
+    // this.renderer.setScissorTest(true);
+    // this.renderer.setViewport(
+    //   this.sizes.width - this.sizes.width / 3,
+    //   this.sizes.height - this.sizes.height / 3,
+    //   this.sizes.width / 3,
+    //   this.sizes.height / 3
+    // );
+    // this.renderer.setScissor(
+    //   this.sizes.width - this.sizes.width / 3,
+    //   this.sizes.height - this.sizes.height / 3,
+    //   this.sizes.width / 3,
+    //   this.sizes.height / 3
+    // );
+    // this.renderer.render(this.scene, this.camera.perspectiveCamera);
+    // this.renderer.setScissorTest(false);
   }
 }
