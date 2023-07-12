78a5448d513110eb1e37dd32d4a0bcd2777a2a2a
ajjohnson <aj132@icloud.com>
2023-07-12 15:12:46 -0500

    Init Portfolio
    Node, Vite, and Tailwind installed and configured

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"], // adjusted to include root directory files
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
