c7f5bd8504bd09498fcd431348aa9a49d59883aa
ajjohnson <aj132@icloud.com>
2023-08-15 13:32:18 -0400

    Updated Experience Section

--- main.js
+++ main.js
@@ -72,3 +72,19 @@ var button = document.getElementById("learn-more-proserv");
 button.addEventListener("click", function () {
   window.location.href = "ProServ/";
 });
+
+document.querySelectorAll(".tab").forEach((tab) => {
+  tab.addEventListener("click", function () {
+    document
+      .querySelectorAll(".tab")
+      .forEach((innerTab) => innerTab.classList.remove("active"));
+    tab.classList.add("active");
+
+    document.querySelectorAll(".content-section").forEach((section) => {
+      section.setAttribute("hidden", "true"); // Hide all content sections
+    });
+
+    var tabId = tab.getAttribute("data-tab");
+    document.getElementById(`content-${tabId}`).removeAttribute("hidden"); // Show the corresponding content section
+  });
+});
